6d5b95c21848b8e0cb9e4963af6809e82dd350d2
alex <ataffe@asu.edu>
2019-04-28 22:51:51 -0700

    Fixed bugs in light sensor file.

--- light_sensor.py
+++ light_sensor.py
@@ -2,45 +2,45 @@ import RPi.GPIO as GPIO
 import keyboard
 import threading
 import datetime
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-#WAIT_TIME_SECONDS = 900
-WAIT_TIME_SECONDS = 2
+WAIT_TIME_SECONDS = 900
+#WAIT_TIME_SECONDS = 2
 
-def check_sunlight:
+def check_sunlight():
 	try:
 		f = open("sunlightLog.txt", "a+")
 		timeStamp = time.time()
 		dateTimeString = datetime.datetime.fromtimestamp(timeStamp).strftime('%Y-%m-%d %H:%M:%S')
 		if GPIO.input(4):
-			f.write("NO Sunlight at: " + dateTimeString)
+			f.write("NO Sunlight at: " + dateTimeString + "\n")
 			print "NO SunLight at: " + dateTimeString
 		else:
-			f.write("YES Sunlight at: " + dateTimeString)
+			f.write("YES Sunlight at: " + dateTimeString  + "\n")
 			print "YES SunLight at: " + dateTimeString
+	except Exception as e:
+		print "There was an error writing to file."
+		print e
 	finally:
 		f.close()
-	except:
-		print "There was an error writing to file."
-		
 
-
-def run_continuous:
+def run_continuous():
 	while True:
-	if GPIO.input(4):
-		print "no light"
-	else:
-		print "light"
-	try:
-		if keyboard.is_pressed('q'):
-			print "Exiting"
-			break
+		if GPIO.input(4):
+			print "no light"
 		else:
-			pass
-	except:
-		break
+			print "light"
+		try:
+			if keyboard.is_pressed('q'):
+				print "Exiting"
+				break
+			else:
+				pass
+		except:
+			break
 
 timer = threading.Event()
-while not timer.wait(WAIT_TIME_SECONDS)
+while not timer.wait(WAIT_TIME_SECONDS):
 	check_sunlight()
