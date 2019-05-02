900b9c5b87e969f2952c9f3b5a488b0655aeca54
Alex <ataffe@asu.edu>
2019-05-01 22:44:33 -0700

    Added email test file

--- email_desktop.py
+++ email_desktop.py
@@ -1,46 +1,76 @@
 #! /usr/bin/python
 
-import smtplib
+import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.utils import formatdate
+from email.mime.base import MIMEBase
+from email import encoders
+from os.path import basename
 
+def send_email():
 	port = 465 # For SSL
 	password = "al.EX.91.27"
 	sender_email = "raspberry.pi.taffe@gmail.com"
-    receiver_email = "taffeAlexander@gmail.com"
+	receiver_email = "taffeAlexander@gmail.com"
 	message = MIMEMultipart("alternative")
 	message["Subject"] = "Garden update: " + formatdate(localtime=True)
 	message["From"] = sender_email
-	message["To"] = COMMASPACE.join(receiver_email)
+	message["To"] = receiver_email
 	message["Date"] = formatdate(localtime=True)
 
-    # Create the body of the message (a plain-text and an HTML version).
-text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
-html = """\
-<html>
-  <head></head>
-  <body>
-    <p>Hi!<br>
-       How are you?<br>
-       Here is the <a href="http://www.python.org">link</a> you wanted.
-    </p>
-  </body>
-</html>
-"""
 
-# Record the MIME types of both parts - text/plain and text/html.
-part1 = MIMEText(text, 'plain')
-part2 = MIMEText(html, 'html')
 
-# Attach parts into message container.
-# According to RFC 2046, the last part of a multipart message, in this case
-# the HTML message, is best and preferred.
-message.attach(part1)
-message.attach(part2)
+	# Create the body of the message (a plain-text and an HTML version).
+	text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
+
+	try:
+		f=open("email.html", "r")
+		if f.mode == 'r':
+				html = f.read()
+		f.close()
+
+		#Open the file to be sent
+		attachment = open("sunlightLog.txt", "rb")
+
+		p = MIMEBase('application', 'octet-stream')
+
+		p.set_payload((attachment).read())
+
+		encoders.encode_base64(p)
+
+		p.add_header('Content-Disposition', "attachment; filename=%s" % "sunlightLog.txt")
+		message.attach(p)
+	except Exception as e:
+		print("There was an error reading html file. Defaulting to basic html page")
+		html = """\
+		<html>
+			<head></head>
+			<body>
+				<h1>Garden Update</h1>
+			</body>
+		</html>
+		"""
+		print(e)
+
+
+
+
+	# Record the MIME types of both parts - text/plain and text/html.
+	part1 = MIMEText(text, 'plain')
+	part2 = MIMEText(html, 'html')
+
+	# Attach parts into message container.
+	# According to RFC 2046, the last part of a multipart message, in this case
+	# the HTML message, is best and preferred.
+	message.attach(part1)
+	message.attach(part2)
 
 	# Create a secure SSL context
 	context = ssl.create_default_context()
-	with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+	with smtplib.SMTP_SSL("smtp.gmail.com", port) as server:
 		server.login("raspberry.pi.taffe@gmail.com", password)
 		server.sendmail(sender_email, receiver_email, message.as_string())
 		print("Email Sent")
+
+send_email()
