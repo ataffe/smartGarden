bb0d18997a1ea8524c0b4441330a7cd4e2d9d3b0
alex <ataffe@asu.edu>
2019-05-09 23:23:13 -0700

    Added zip compression

--- test/camera_test.py
+++ test/camera_test.py
@@ -1,11 +1,20 @@
 import os
+import zipfile
 from datetime import datetime
 
 filename = str(datetime.now()).replace(" ", "-")
 filename = filename.replace(":","-")
 filename = filename.replace(".","-")
+filename = filename + ".jpg"
 print("filename: " + filename)
-myCmd = 'fswebcam -r 1280x720 /home/pi/Desktop/smartGarden/smartGarden/test/images/' + str(filename) + '.jpg'
-scp_command = "scp images/" + filename + ".jpg alex@192.168.0.7:C:\\\\Users\\\\Alex\\\\Desktop\\\\SmartGarden\\\\"
+myCmd = 'fswebcam -r 1280x720 /home/pi/Desktop/smartGarden/smartGarden/test/images/' + str(filename)
 os.system(myCmd)
+zf = zipfile.ZipFile("images/" + filename + ".zip", mode = 'w')
+try:
+	zf.write("images/" + filename, compress_type= zipfile.ZIP_DEFLATED)
+finally:
+	zf.close()
+os.system("rm images/*.jpg")
+
+scp_command = "scp images/" + filename + ".zip alex@192.168.0.7:C:\\\\Users\\\\Alex\\\\Desktop\\\\SmartGarden\\\\"
 os.system(scp_command)
