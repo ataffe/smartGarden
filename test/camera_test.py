a1b6b3ac8792f22ceb521f0cd4a100f665f02b65
alex <ataffe@asu.edu>
2019-05-09 23:04:32 -0700

    Figured out SCP to Windowsgit add .git add .git add .

--- test/camera_test.py
+++ test/camera_test.py
@@ -6,4 +6,6 @@ filename = filename.replace(":","-")
 filename = filename.replace(".","-")
 print("filename: " + filename)
 myCmd = 'fswebcam -r 1280x720 /home/pi/Desktop/smartGarden/smartGarden/test/images/' + str(filename) + '.jpg'
+scp_command = "scp images/" + filename + ".jpg alex@192.168.0.7:C:\\\\Users\\\\Alex\\\\Desktop\\\\SmartGarden\\\\"
 os.system(myCmd)
+os.system(scp_command)
