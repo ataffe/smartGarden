3afa8959b1f77fbd0f4e485eea1e3f8bdd824995
Alex <taffeAlexander@gmail.com>
2019-05-14 13:09:44 -0700

    Update smartGarden.py
    Added soil moisture to email.

--- smartGarden.py
+++ smartGarden.py
@@ -185,25 +185,49 @@ def send_email():
 						<tr>
 							<th style="font-size: medium;padding: 8px;background-color: #4CAF50;color: white;"> Sunlight </th>
 							<th style="font-size: medium;padding: 8px;background-color: #4CAF50;color: white;"> TimeStamp </th>
+                            <th style="font-size: medium;padding: 8px;background-color: #4CAF50;color: white;"> Soil Moisture </th>
+                            <th style="font-size: medium;padding: 8px;background-color: #4CAF50;color: white;"> TimeStamp </th>
 						</tr>
 						"""
+        soilLogArray = []
+		with open("/home/pi/Desktop/smartGarden/smartGarden/soilLog.txt", "r") as fp2:
+            for count, line in enumerate(fp2):
+                soilLogArray.append(line)
+		
 		with open("/home/pi/Desktop/smartGarden/smartGarden/sunlightLog.txt", "r") as fp:
 			for cnt, line in enumerate(fp):
 				lineArray = line.split()
 				currentYMD = str(datetime.now()).split()[0]
+                soilMoisture = "No Data"
+                soilTimeStamp = "No Data"
+
+                if cnt < len(soilLogArray) {
+                    try:
+                        soilMoisture = soilLogArray[cnt].split()[3]
+                        soilTimeStamp = soilLogArray[cnt].split()[4] + " " + soilLogArray[cnt].split()[5]
+                    catch Exception as e:
+                        logging.warn("Unable to parse soil moisture or time stamp for email.")
+                        logging.warn(e)
+                }
+
 				if currentYMD == lineArray[3]:
 					if cnt % 2 == 0:
 						if "YES" in lineArray[0]:
 							row = "<tr><td style='color: #FFD700;background-color: #00aced;border: 1px solid;padding: 8px; text-align: center; '>" + lineArray[0] + " " + lineArray[1] + "</td>"
 						else:
 							row = "<tr><td style='border: 1px solid;padding: 8px; text-align: center; '>" + lineArray[0] + " " + lineArray[1] + "</td>"
-						row = row + "<td style='border: 1px solid;padding: 8px; text-align: center'>" + lineArray[3] + " " +  lineArray[4]+ "</td></tr>"
+						
+                        row = row + "<td style='border: 1px solid;padding: 8px; text-align: center'>" + lineArray[3] + " " +  lineArray[4]+ "</td>"
+                        row = row + "<td style='border: 1px solid;padding: 8px; text-align: center; '>" + soilMoisture + "</td>"
+                        row = row + "<td style='border: 1px solid;padding: 8px; text-align: center; '>" + soilTimeStamp + "</td></tr>"
 					else:
 						if "YES" in lineArray[0]:
 							row = "<tr><td style='color: #FFD700;background-color: #00aced;border: 1px solid;padding: 8px; text-align: center; '>" + lineArray[0] + " " + lineArray[1] + "</td>"
 						else:
 							row = "<tr><td style='background-color: #f2f2f2;border: 1px solid;padding: 8px; text-align: center; '>" + lineArray[0] + " " + lineArray[1] + "</td>"
-						row = row + "<td style='background-color: #f2f2f2;border: 1px solid;padding: 8px; text-align: center'>" + lineArray[3] + " " +  lineArray[4]+ "</td></tr>"
+						row = row + "<td style='background-color: #f2f2f2;border: 1px solid;padding: 8px; text-align: center'>" + lineArray[3] + " " +  lineArray[4]+ "</td>"
+                        row = row + "<td style='border: 1px solid;padding: 8px; text-align: center; '>" + soilMoisture + "</td>"
+                        row = row + "<td style='border: 1px solid;padding: 8px; text-align: center; '>" + soilTimeStamp + "</td></tr>"
 					html = html + row
 				elif currentYMD == lineArray[4]:
 					if cnt % 2 == 0:
@@ -211,13 +235,17 @@ def send_email():
 							row = "<tr><td style='color: #FFD700;background-color: #00aced;border: 1px solid;padding: 8px; text-align: center; '>" + lineArray[0] + " " + lineArray[1] + " " + lineArray[2] + "</td>"
 						else:
 							row = "<tr><td style='border: 1px solid;padding: 8px; text-align: center; '>" + lineArray[0] + " " + lineArray[1] + "</td>"
-						row = row + "<td style='border: 1px solid;padding: 8px; text-align: center'>" + lineArray[4] + " " +  lineArray[5]+ "</td></tr>"
+						row = row + "<td style='border: 1px solid;padding: 8px; text-align: center'>" + lineArray[4] + " " +  lineArray[5]+ "</td>"
+                        row = row + "<td style='border: 1px solid;padding: 8px; text-align: center; '>" + soilMoisture + "</td>"
+                        row = row + "<td style='border: 1px solid;padding: 8px; text-align: center; '>" + soilTimeStamp + "</td></tr>"
 					else:
 						if "YES" in lineArray[0]:
 							row = "<tr><td style='color: #FFD700;background-color: #00aced;border: 1px solid;padding: 8px; text-align: center; '>" + lineArray[0] + " " + lineArray[1] + " " + lineArray[2] + "</td>"
 						else:
 							row = "<tr><td style='background-color: #f2f2f2;border: 1px solid;padding: 8px; text-align: center; '>" + lineArray[0] + " " + lineArray[1] + "</td>"
-						row = row + "<td style='background-color: #f2f2f2;border: 1px solid;padding: 8px; text-align: center'>" + lineArray[4] + " " +  lineArray[5]+ "</td></tr>"
+						row = row + "<td style='background-color: #f2f2f2;border: 1px solid;padding: 8px; text-align: center'>" + lineArray[4] + " " +  lineArray[5]+ "</td>"
+                        row = row + "<td style='border: 1px solid;padding: 8px; text-align: center; '>" + soilMoisture + "</td>"
+                        row = row + "<td style='border: 1px solid;padding: 8px; text-align: center; '>" + soilTimeStamp + "</td></tr>"
 					html = html + row
 		html = html + """\
 					</table>
@@ -340,7 +368,7 @@ def check_soil():
 	
 	try:
 		f = open("/home/pi/Desktop/smartGarden/smartGarden/soilLog.txt", "a+")
-		f.write("Soil Moisture Level: " + str(rawVal) + "\n")
+		f.write("Soil Moisture Level: " + str(rawVal) + " " + str(datetime.now()) + "\n")
 	except Exception as e:
 		logging.WARN("Error writing soil moisture level")
 		logging.WARN(e)
