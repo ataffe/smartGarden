b1921e732d004c6a0f33cb23d02bd314147d0175
Alex <taffeAlexander@gmail.com>
2020-02-24 13:05:24 -0700

    Update pump.py
    Created water pump class

--- GardenModules/pump/pump.py
+++ GardenModules/pump/pump.py
@@ -3,33 +3,38 @@ import logging
 import time
 from datetime import datetime
 
-def run_pump(run_time=None,pwm=50):
-
-	if run_time == None:
-		raise Exception("The value of run_time for the water pump was  None")
-	try:
-		dutycycle = 60
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(18, GPIO.OUT)
-		GPIO.output(18, GPIO.HIGH)
-		GPIO.output(18, GPIO.LOW)
-		p = GPIO.PWM(18,pwm)
-		p.start(dutycycle)
-		time.sleep(run_time)
-		GPIO.output(18, GPIO.LOW)
-		p.stop()
-		logging.info("Watered plants at: " + str(datetime.now()))
-		logging.info(""" 
-		
-		                                ,d                          
-                                88                          
-8b      db      d8 ,adPPYYba, MM88MMM ,adPPYba, 8b,dPPYba,  
-`8b    d88b    d8' ""     `Y8   88   a8P_____88 88P'   "Y8  
- `8b  d8'`8b  d8'  ,adPPPPP88   88   8PP""""""" 88          
-  `8bd8'  `8bd8'   88,    ,88   88,  "8b,   ,aa 88          
-    YP      YP     `"8bbdP"Y8   "Y888 `"Ybbd8"' 88  
+class WaterPump:
+    _dutyCycle = 60
+    _pin = 18
+    
+    def run(self,runtime=None, pwm=50):
+        if runtime == None:
+            raise Exception("The value of run_time for the water pump was  None")
+        try:
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(_pin, GPIO.OUT)
+            GPIO.output(_pin, GPIO.HIGH)
+            GPIO.output(_pin, GPIO.LOW)
+            p = GPIO.PWM(_pin,pwm)
+            p.start(_dutyCycle)
+            time.sleep(runtime)
+            GPIO.output(_pin, GPIO.LOW)
+            p.stop()
+            logging.info("Watered plants at: " + str(datetime.now()))
+        except Exception as exception:
+            logging.warn("There was an error watering the plants.")
+            logging.warn(exception)
+            _printWatered(self)
 	
-	""")
-	except Exception as e:
-		logging.warn("There was an error watering the plants.")
-		logging.warn(e)
+    def _printWatered(self):
+        logging.info(""" 
+		
+                                            ,d                          
+                                    88                          
+    8b      db      d8 ,adPPYYba, MM88MMM ,adPPYba, 8b,dPPYba,  
+    `8b    d88b    d8' ""     `Y8   88   a8P_____88 88P'   "Y8  
+    `8b  d8'`8b  d8'  ,adPPPPP88   88   8PP""""""" 88          
+    `8bd8'  `8bd8'   88,    ,88   88,  "8b,   ,aa 88          
+        YP      YP     `"8bbdP"Y8   "Y888 `"Ybbd8"' 88  
+        
+        """)
