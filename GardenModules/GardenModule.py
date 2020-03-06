97f4a6cfb45cf6c57db25ec62a27e4cbf119b850
Alex <taffeAlexander@gmail.com>
2020-03-06 13:04:25 -0700

    Update GardenModule.py
    added sentinel.

--- GardenModules/GardenModule.py
+++ GardenModules/GardenModule.py
@@ -2,13 +2,13 @@ from threading import Thread
 
 
 class GardenModule(Thread):
-	def __init__(self):
+	def __init__(self, event):
 		super().__init__()
-		self._shutDownFlag = False
+		self._sentinel = event
 		self.daemon = True
 
 	def run(self):
 		pass
 
 	def shutdown(self):
-		self._shutDownFlag = True
+		self._sentinel.set()
