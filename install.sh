4bbea774de7df9f7fab6a006f9a2fbf1a40b63c9
alex <ataffe@asu.edu>
2020-11-21 17:41:25 -0700

    Added circuit python image, install script, and lux sensor

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#sudo pip3 install adafruit-circuitpython-ads1x15
+#sudo pip3 install flask
+#sudo pip3 install Adafruit-Blinka
+pip3 install opencv-python
+sudo apt-get install libcblas-dev
+sudo apt-get install libhdf5-dev
+sudo apt-get install libhdf5-serial-dev
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libjasper-dev
+sudo apt-get install libqtgui4
+sudo apt-get install libqt4-test
