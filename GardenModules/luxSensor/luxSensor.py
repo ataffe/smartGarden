f8961c8fd898a4bfeebd85dffa825cd536961839
ataffe <ataffe@asu.edu>
2020-11-29 01:46:57 -0700

    added lux sensor module

---
+++ GardenModules/luxSensor/luxSensor.py
@@ -0,0 +1,40 @@
+import adafruit_tsl2561
+import busio
+import board
+import threading
+from GardenModules.GardenModule import GardenModule
+
+
+class LuxSensor(GardenModule):
+    def __init__(self, log, queue):
+        super().__init__(queue)
+        self._logging = log
+        self._i2c = busio.I2C(board.SCL, board.SDA)
+        self._sensor = adafruit_tsl2561.TSL2561(self._i2c)
+        self._sensor.gain = 0
+        self._grow_light_lux = 535
+        self._lux_interval = 300
+
+    def getLux(self):
+        return self._sensor.lux
+
+    def isSunlight(self):
+        return self._sensor.lux > self._grow_light_lux
+
+    def run(self):
+        timer = threading.Event()
+        while not timer.wait(self._lux_interval):
+            self._logging.info(self)
+            print(self)
+
+            if self._sentinel.get(block=True):
+                print("Sentinel was triggered in soil thread.")
+                self._sentinel.put(True)
+                self._sentinel.task_done()
+                break
+            self._sentinel.put(False)
+            self._sentinel.task_done()
+
+    def __str__(self):
+        return "Current lux reading: {}".format(self.getLux())
+
--- smartGarden.py
+++ smartGarden.py
@@ -1,12 +1,10 @@
-import RPi.GPIO as GPIO
 import threading
 from datetime import datetime
 from datetime import timedelta
-import time
 import os
 import zipfile
 import logging
-import GardenModules.sunlightSensor.sunlight as sunlight
+from GardenModules.luxSensor.luxSensor import LuxSensor
 from GardenModules.pump.pump import WaterPump
 from GardenModules.soilMoisture.soil import SoilMoisture
 from GardenModules.gardenServer.gardenServer import GardenServer
@@ -32,161 +30,168 @@ WAIT_TIME_PRUNE = 86400
 image_count = 0
 SHUTDOWN_FLAG = False
 
-
 app = Flask(__name__, template_folder='/home/pi/Desktop/smartGarden/smartGarden/ControlPanel',
             static_folder="/home/pi/Desktop/smartGarden/smartGarden/ControlPanel")
 CORS(app)
 Debug(app)
 
+
 def create_folder():
-	filename = str(datetime.now()).replace(" ", "-")
-	dateArray = filename.split('-')
-	ymd = dateArray[0] + "-" + dateArray[1] + "-" + dateArray[2]
-	try:
-		os.mkdir("/home/pi/Desktop/smartGarden/smartGarden/images/" + ymd)
-	except FileExistsError:
-		pass
-	finally:
-		return ymd
+    filename = str(datetime.now()).replace(" ", "-")
+    dateArray = filename.split('-')
+    ymd = dateArray[0] + "-" + dateArray[1] + "-" + dateArray[2]
+    try:
+        os.mkdir("/home/pi/Desktop/smartGarden/smartGarden/images/" + ymd)
+    except FileExistsError:
+        pass
+    finally:
+        return ymd
+
 
 def zipdir(path, ziph):
-	logging.info("zipping path: " + path)
-	for root, dirs, files in os.walk(path):
-		logging.info("root: " + root)
-		for file in files:
-			ziph.write(os.path.join(root, file))
+    logging.info("zipping path: " + path)
+    for root, dirs, files in os.walk(path):
+        logging.info("root: " + root)
+        for file in files:
+            ziph.write(os.path.join(root, file))
+
 
 def send_folder(ymd):
-	time1 = datetime.now()
-	logging.info("Zipping File... "+ str(datetime.now()))
-	baseFolder = ymd
-	baseFolder = "/home/pi/Desktop/smartGarden/smartGarden/images/" + baseFolder
-	ymd = baseFolder + ".zip"
-	currentDirectory = os.path.dirname(os.path.realpath(__file__))
-	os.chdir("/home/pi/Desktop/smartGarden/smartGarden/images")
-	zf = zipfile.ZipFile(ymd, mode = 'w', compression=zipfile.ZIP_LZMA)
-	try:
-		zipdir(baseFolder,zf)
-	finally:
-		zf.close()
-	logging.info("Sending images...")
-	try:
-		scp_command = "SSHPASS='al.EX.91.27' sshpass -e scp " + ymd + " alext@192.168.0.20:D:\\\\smartGarden\\\\Images"
-	except Exception as e:
-		logging.warn("There was an error sending the file to Cacutar")
-		logging.warn(e)
-	try:
-		os.system(scp_command)
-		os.system("rm " + ymd)
-		#os.system("rm -r " + baseFolder)
-		os.chdir(currentDirectory)
-		time2 = datetime.now()
-		diff = time2 - time1
-		logging.info("It took (mins, seconds): " + str(divmod(diff.total_seconds(),60)) + " to transfer " + str(ymd))
-	except Exception as e:
-		logging.warn("There was an error deleting the folders and moving back a directory")
-		logging.warn(e)
+    time1 = datetime.now()
+    logging.info("Zipping File... " + str(datetime.now()))
+    baseFolder = ymd
+    baseFolder = "/home/pi/Desktop/smartGarden/smartGarden/images/" + baseFolder
+    ymd = baseFolder + ".zip"
+    currentDirectory = os.path.dirname(os.path.realpath(__file__))
+    os.chdir("/home/pi/Desktop/smartGarden/smartGarden/images")
+    zf = zipfile.ZipFile(ymd, mode='w', compression=zipfile.ZIP_LZMA)
+    try:
+        zipdir(baseFolder, zf)
+    finally:
+        zf.close()
+    logging.info("Sending images...")
+    try:
+        scp_command = "SSHPASS='al.EX.91.27' sshpass -e scp " + ymd + " alext@192.168.0.20:D:\\\\smartGarden\\\\Images"
+    except Exception as e:
+        logging.warn("There was an error sending the file to Cacutar")
+        logging.warn(e)
+    try:
+        os.system(scp_command)
+        os.system("rm " + ymd)
+        # os.system("rm -r " + baseFolder)
+        os.chdir(currentDirectory)
+        time2 = datetime.now()
+        diff = time2 - time1
+        logging.info("It took (mins, seconds): " + str(divmod(diff.total_seconds(), 60)) + " to transfer " + str(ymd))
+    except Exception as e:
+        logging.warn("There was an error deleting the folders and moving back a directory")
+        logging.warn(e)
+
 
 def take_pics(ymd, number=1):
-	for x in range(number):
-		logging.info("Taking image " + str(x + 1) + " out of " + str(number))
-		filename = str(datetime.now()).replace(" ", "-")
-		filename = filename.replace(":","-")
-		filename = filename.replace(".","-")
-		filename = filename + ".jpg"
-		#Take image
-		#old was 800x600
-		vid_cap = cv2.VideoCapture(0)
-		vid_cap.set(3, 1280)
-		vid_cap.set(4, 720)
-		if not vid_cap.isOpened():
-			logging.warn("Error opening video device using opencv")
-		else:
-			print("Taking picture")
-			for x in range(10):
-				ret, image = vid_cap.read()
-			cv2.imwrite("/home/pi/Desktop/smartGarden/smartGarden/images/" + ymd + "/" + str(filename), image)
-			vid_cap.release()
-			#print("Sending picture to: " + "/home/pi/Desktop/smartGarden/smartGarden/images/" + ymd + "/" + str(filename))
-		#myCmd = 'fswebcam -q -i 0 -r 1280x720 /home/pi/Desktop/smartGarden/smartGarden/images/' + ymd + "/" + str(filename)
-		#os.system(myCmd)
+    for x in range(number):
+        logging.info("Taking image " + str(x + 1) + " out of " + str(number))
+        filename = str(datetime.now()).replace(" ", "-")
+        filename = filename.replace(":", "-")
+        filename = filename.replace(".", "-")
+        filename = filename + ".jpg"
+        # Take image
+        # old was 800x600
+        vid_cap = cv2.VideoCapture(0)
+        vid_cap.set(3, 1280)
+        vid_cap.set(4, 720)
+        if not vid_cap.isOpened():
+            logging.warn("Error opening video device using opencv")
+        else:
+            print("Taking picture")
+            for x in range(10):
+                ret, image = vid_cap.read()
+            cv2.imwrite("/home/pi/Desktop/smartGarden/smartGarden/images/" + ymd + "/" + str(filename), image)
+            vid_cap.release()
+        # print("Sending picture to: " + "/home/pi/Desktop/smartGarden/smartGarden/images/" + ymd + "/" + str(filename))
+    # myCmd = 'fswebcam -q -i 0 -r 1280x720 /home/pi/Desktop/smartGarden/smartGarden/images/' + ymd + "/" + str(filename)
+    # os.system(myCmd)
+
 
 def run_camera(send_folder):
-	ymd = create_folder()
-	if send_folder:
-		logging.info("Sending folder")
-		yesterday = datetime.now() - timedelta(days=1)
-		filename = str(yesterday).replace(" ", "-")
-		dateArray = filename.split('-')
-		ymd = dateArray[0] + "-" + dateArray[1] + "-" + dateArray[2]
-		send_folder(ymd)
-		ymd = create_folder()
-	take_pics(ymd)
+    ymd = create_folder()
+    if send_folder:
+        logging.info("Sending folder")
+        yesterday = datetime.now() - timedelta(days=1)
+        filename = str(yesterday).replace(" ", "-")
+        dateArray = filename.split('-')
+        ymd = dateArray[0] + "-" + dateArray[1] + "-" + dateArray[2]
+        send_folder(ymd)
+        ymd = create_folder()
+    take_pics(ymd)
+
 
 def camera_thread():
-	#TODO ADD ANOTHER THREAD FOR SENDING IMAGES TO CACTUAR PC
-	ymd = create_folder()
-	timer = threading.Event()
-	#run_camera(send_folder=False)
-	send_folder = False
-	sent_folder = False
-	while not timer.wait(CAMERA_TIME_SECONDS) and not SHUTDOWN_FLAG:
-		try:
-			time = str(datetime.now()).split()
-			hour = str(time[1].split(':')[0])
-		except Exception as e:
-			print("Error parsing date for camera.")
-			print(e)
-		if hour == "00" and not sent_folder:
-			send_folder = True
-			sent_folder = True
-		elif hour != "00" and sent_folder:
-			sent_folder = False
-		else:
-			send_folder = False
-		run_camera(False)
-		if SHUTDOWN_FLAG:
-			break
+    # TODO ADD ANOTHER THREAD FOR SENDING IMAGES TO CACTUAR PC
+    ymd = create_folder()
+    timer = threading.Event()
+    # run_camera(send_folder=False)
+    send_folder = False
+    sent_folder = False
+    while not timer.wait(CAMERA_TIME_SECONDS) and not SHUTDOWN_FLAG:
+        try:
+            time = str(datetime.now()).split()
+            hour = str(time[1].split(':')[0])
+        except Exception as e:
+            print("Error parsing date for camera.")
+            print(e)
+        if hour == "00" and not sent_folder:
+            send_folder = True
+            sent_folder = True
+        elif hour != "00" and sent_folder:
+            sent_folder = False
+        else:
+            send_folder = False
+        run_camera(False)
+        if SHUTDOWN_FLAG:
+            break
 
 
 def prune_logs_thread():
-	#prune.prune("smartGardenLog.txt")
-	timer = threading.Event()
-	while not timer.wait(WAIT_TIME_PRUNE) and not SHUTDOWN_FLAG:
-		#TODO FIX PRUNING
-		print("Pruning smartGardenLog")
-		prune.prune("smartGardenLog.txt")
-		prune("soilLog.txt")
-		#prune("sunlightLog.txt")
-		if SHUTDOWN_FLAG:
-			break
+    # prune.prune("smartGardenLog.txt")
+    timer = threading.Event()
+    while not timer.wait(WAIT_TIME_PRUNE) and not SHUTDOWN_FLAG:
+        # TODO FIX PRUNING
+        print("Pruning smartGardenLog")
+        prune.prune("smartGardenLog.txt")
+        prune("soilLog.txt")
+        # prune("sunlightLog.txt")
+        if SHUTDOWN_FLAG:
+            break
 
 
 if __name__ == "__main__":
-	logging.basicConfig(filename="/home/pi/Desktop/smartGarden/smartGarden/logs/smartGardenLog.log", level=logging.INFO)
-	sentinel = queue.Queue()
-	sentinel.put(False)
+    logging.basicConfig(filename="/home/pi/Desktop/smartGarden/smartGarden/logs/smartGardenLog.log", level=logging.INFO)
+    sentinel = queue.Queue()
+    sentinel.put(False)
 
-	soilMoistureSensor = SoilMoisture(logging, sentinel)
-	pump = WaterPump(logging, sentinel, soilMoistureSensor)
-	server = GardenServer(pump, sentinel)
-	artificialLight = ArtificialLight(logging, sentinel)
-	signal.signal(signal.SIGINT, server.shutDownGarden)
+    soilMoistureSensor = SoilMoisture(logging, sentinel)
+    pump = WaterPump(logging, sentinel, soilMoistureSensor)
+    server = GardenServer(pump, sentinel)
+    luxSensor = LuxSensor(logging, sentinel)
+    artificialLight = ArtificialLight(logging, sentinel)
+    signal.signal(signal.SIGINT, server.shutDownGarden)
 
-	# thread4 = threading.Thread(target=camera_thread)
-	thread8 = threading.Thread(target=prune_logs_thread)
-	
-	print("Starting threads at time: " + str(datetime.now()) + "...")
-	logging.info("Starting threads at time: " + str(datetime.now()) + "...")
+    # thread4 = threading.Thread(target=camera_thread)
+    thread8 = threading.Thread(target=prune_logs_thread)
 
-	pump.start()
-	artificialLight.start()
-	soilMoistureSensor.start()
-	server.start()
+    print("Starting threads at time: " + str(datetime.now()) + "...")
+    logging.info("Starting threads at time: " + str(datetime.now()) + "...")
 
-	thread8.daemon = True
-	thread8.start()
-	print("""  
+    pump.start()
+    luxSensor.start()
+    artificialLight.start()
+    soilMoistureSensor.start()
+    server.start()
+
+    thread8.daemon = True
+    thread8.start()
+    print("""  
  ____                       _      ____               _            
 / ___| _ __ ___   __ _ _ __| |_   / ___| __ _ _ __ __| | ___ _ __  
 \___ \| '_ ` _ \ / _` | '__| __| | |  _ / _` | '__/ _` |/ _ \ '_ \ 
@@ -197,7 +202,7 @@ if __name__ == "__main__":
   Version 1.0
   \n\n\nAll Threads Started!\n\n\n
   """)
-	logging.info("""
+    logging.info("""
  ____                       _      ____               _            
 / ___| _ __ ___   __ _ _ __| |_   / ___| __ _ _ __ __| | ___ _ __  
 \___ \| '_ ` _ \ / _` | '__| __| | |  _ / _` | '__/ _` |/ _ \ '_ \ 
@@ -209,21 +214,21 @@ if __name__ == "__main__":
   \n\n\nAll Threads Started!\n\n\n
   """)
 
-	server.join()
-	print("server shutdown")
-	logging.info("Shut Down Complete!")
-	sys.exit()
+    server.join()
+    print("server shutdown")
+    logging.info("Shut Down Complete!")
+    sys.exit()
 
-	pump.join()
-	print("Pump thread ended")
+    pump.join()
+    print("Pump thread ended")
 
-	# thread4.join()
-	# print("Thread 4 ended")
-	artificialLight.join()
-	print("Artificial Light ended")
-	soilMoistureSensor.join()
-	print("Soil Moisture thread ended")
-
-	thread8.join()
-	print("Thread 8 ended")
-
+    # thread4.join()
+    # print("Thread 4 ended")
+    artificialLight.join()
+    print("Artificial light ended")
+    soilMoistureSensor.join()
+    print("Soil moisture thread ended")
+    luxSensor.join()
+    print("Lux sensor thread ended")
+    thread8.join()
+    print("Thread 8 ended")
